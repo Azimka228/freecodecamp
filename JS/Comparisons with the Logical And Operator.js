380617a02cc0bab2317fb5c8d741cd13edba569b
Azimka <71658005+Azimka228@users.noreply.github.com>
2022-07-21 12:58:21 +0300

    Completed task

---
+++ JS/Comparisons with the Logical And Operator.js
@@ -0,0 +1,12 @@
+function testLogicalAnd(val) {
+    // Only change code below this line
+  
+    if (val >= 25 && val <= 50) {
+      return "Yes"
+    }
+  
+    // Only change code above this line
+    return "No";
+  }
+  
+  testLogicalAnd(10);
