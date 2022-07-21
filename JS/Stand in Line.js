ddfa4d00d43fc988a7c81a135546bc649682cac8
Azimka <71658005+Azimka228@users.noreply.github.com>
2022-07-21 12:31:04 +0300

    Completed task

---
+++ JS/Stand in Line.js
@@ -0,0 +1,13 @@
+function nextInLine(arr, item) {
+    arr.push(item)
+    return arr.shift();
+    // Only change code above this line
+  }
+  
+  // Setup
+  const testArr = [1, 2, 3, 4, 5];
+  
+  // Display code
+  console.log("Before: " + JSON.stringify(testArr));
+  console.log(nextInLine(testArr, 6));
+  console.log("After: " + JSON.stringify(testArr));
