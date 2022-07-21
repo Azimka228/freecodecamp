1a8870b9a2041d32fbcba3720f21155f888947c6
Azimka <71658005+Azimka228@users.noreply.github.com>
2022-07-21 12:03:17 +0300

    Completed task

---
+++ JS/Understanding Undefined Value returned from a Function.js
@@ -0,0 +1,16 @@
+// Setup
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+function addFive() {
+  sum += 5;
+}
+// Only change code below this line
+
+
+// Only change code above this line
+
+addThree();
+addFive();
