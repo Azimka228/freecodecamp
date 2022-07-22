6efb1a2e8db1e708ad3db2040dd5693c3b8fe60f
Azimka <71658005+Azimka228@users.noreply.github.com>
2022-07-22 22:41:22 +0300

    Completed task

---
+++ JS/Manipulating Complex Objects.js
@@ -0,0 +1,24 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+     {
+      "artist": "Billy JIN",
+      "title": "Pianii bich",
+      "release_year": 1999,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    }
+  ];
