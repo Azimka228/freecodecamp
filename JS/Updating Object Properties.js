a58ce1ae39a1e6c77ee92d3ba0661b396d99ad85
Azimka <71658005+Azimka228@users.noreply.github.com>
2022-07-22 21:54:41 +0300

    Completed task

---
+++ JS/Updating Object Properties.js
@@ -0,0 +1,10 @@
+// Setup
+const myDog = {
+    "name": "Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  myDog.name = "Happy Coder"
+  // Only change code below this line
+  
