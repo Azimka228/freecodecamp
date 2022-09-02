fc8e5bcf68593d86247e5de78c9e4cee7f636296
Azimka <71658005+Azimka228@users.noreply.github.com>
2022-09-02 22:48:53 +0300

    Completed task

---
+++ JS/Understand Own Properties.js
@@ -0,0 +1,15 @@
+function Bird(name) {
+    this.name = name;
+    this.numLegs = 2;
+  }
+  
+  let canary = new Bird("Tweety");
+  let ownProps = [];
+  for (let property in canary) {
+    if(canary.hasOwnProperty(property)) {
+      ownProps.push(property);
+    }
+  }
+  
+  console.log(ownProps);
+  // Only change code below this line
